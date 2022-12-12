b88662cc2141144b0db37be40821d6cd5edcf626
bhadawi404 <abadawi.911@gmail.com>
2022-12-13 01:12:24 +0700

    settings(5)

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -141,3 +141,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
