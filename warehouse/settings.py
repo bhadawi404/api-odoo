3071b900f5c41d0c995a5106062c41ed6284d80c
bhadawi404 <abadawi.911@gmail.com>
2022-12-12 23:10:39 +0700

    settings

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wf$nb3u8$@3me5&48iqs--_q*)yri6lfy$1jy3zqp6e@e^g$!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['149.129.233.161']
+ALLOWED_HOSTS = ['149.129.233.161','127.0.0.1']
 
 
 # Application definition
@@ -141,4 +141,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
