3979b5d7b4de4c57c05eb9a5c044b0d943332688
bhadawi404 <abadawi.911@gmail.com>
2022-12-25 01:40:36 +0700

    database

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'warehouse',
         'USER': 'indosoftware',
         'PASSWORD': '080595',
-        'HOST': '149.129.233.161',
+        # 'HOST': 'localhost',
         'PORT': '5432',
     }
 }
