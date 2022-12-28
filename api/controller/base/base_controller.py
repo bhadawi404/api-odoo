8c0fb5222832f4ff019b4e7724dcd6b395c7939f
bhadawi404 <abadawi.911@gmail.com>
2022-12-28 15:33:37 +0700

    limit, offset and get by id master part

--- api/controller/base/base_controller.py
+++ api/controller/base/base_controller.py
@@ -89,9 +89,10 @@ def page(request, controllerName):
     responseDA = controller_response(controllerName)
     data = []
     error_message = []
-
+    limit = request.query_params.get('limit', False)
+    offset = request.query_params.get('offset', False)
     try:
-        response = modelDA.getall(controller)
+        response = modelDA.getall(controller,limit,offset)
         if response == 'Access Denied':
             error_message = 'Access Denied'
             content = {
@@ -183,7 +184,55 @@ def detail(request, barcode, controllerName):
     return Response(status=response_status, data=content)
 
 @api_view(['GET'])
+def detail_id(request, id, controllerName):
+    print("masuk sini")
+    modelDA = BaseDA()
+    modelResponse = BaseResponse()
+    controller = controller_translator(controllerName)
+    responseDA = controller_response(controllerName)
+    data = []
+    total_data = 0
+    error_message = []
+    
+    try:
+        response = modelDA.getbyid(controller,id)
+        if response == 'Access Denied':
+            error_message = 'Access Denied'
+            content = {
+                "error_message": json.loads(json.dumps(error_message, default=lambda o: o.__dict__)),
+            }
+            return Response(status=status.HTTP_403_FORBIDDEN, data=content) 
+        dataResponse = eval(responseDA)(response)
+        
+        if not dataResponse :
+            error_message = 'DATA NOT FOUND'
+            content = {
+                "error_message": json.loads(json.dumps(error_message, default=lambda o: o.__dict__)),
+                "data": json.loads(json.dumps(data, default=lambda o: o.__dict__)),
+                "status": False,
+                "total_data": 0
+            }
+            return Response(status=status.HTTP_200_OK, data=content)
+        data = dataResponse
+        # data = response
+        error_message = []
+    except Exception as ex:
+        error_message.append(str(ex))
+
+    content = {
+        "statusCode": 200,
+        "statusCodeDesc": 'OK',
+        "data": json.loads(json.dumps(data, default=lambda o: o.__dict__)),
+        
+    }
+
+    response_status = (status.HTTP_200_OK if content["statusCode"] == 200 else status.HTTP_400_BAD_REQUEST)
+
+    return Response(status=response_status, data=content)
+
+@api_view(['GET'])
 def scan(request, controllerName):
+    print("bisa")
     barcode = request.data['barcode']
     modelDA = BaseDA()
     modelResponse = BaseResponse()
--- dataacess/da/base_da.py
+++ dataacess/da/base_da.py
@@ -20,7 +20,7 @@ password = '4mti55'
 class BaseDA(object):
    
     #get all
-    def getall(self, controllerName):
+    def getall(self, controllerName, limit=False, offset=False):
         result = []
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
@@ -31,9 +31,14 @@ class BaseDA(object):
             fields_list = []
             for x in fields:
                 fields_list.append(x)
-            
-            response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[]], {'fields': fields_list})
-
+            if limit:
+                response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[]], {'fields': fields_list,'limit':int(limit)})
+            if offset:
+                response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[]], {'fields': fields_list,'offset':int(offset)})
+            if offset and limit:
+                response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[]], {'fields': fields_list,'limit':int(limit),'offset':int(offset)})
+            if not offset and not limit:
+                response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[]], {'fields': fields_list})
             if not response :
                 response = []
 
@@ -67,7 +72,6 @@ class BaseDA(object):
             result = response
         return result
     
-    
     #get all
     def getbyidscan(self, controllerName,barcode):
         result = []
@@ -82,6 +86,30 @@ class BaseDA(object):
                 fields_list.append(x)
             
             response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[['name','=',barcode]]], {'fields': fields_list})
+
+            if not response :
+                response = []
+
+            result = response
+        else:
+            response = 'Access Denied'
+            result = response
+        return result
+    
+    #get id
+    def getbyid(self, controllerName,id):
+        result = []
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        uid = common.authenticate(db, username, password, {})
+        if uid:
+            models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+            fields = models.execute_kw(db, uid, password, controllerName, 'fields_get', [], {'attributes': ['name']})
+            
+            fields_list = []
+            for x in fields:
+                fields_list.append(x)
+            
+            response =models.execute_kw(db, uid, password, controllerName, 'search_read', [[['id','=',id]]], {'fields': fields_list})
 
             if not response :
                 response = []
