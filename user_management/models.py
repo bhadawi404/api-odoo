d1dc760b5a87b3df70cf7c38d17476e4c319997e
bhadawi404 <abadawi.911@gmail.com>
2022-12-28 22:50:38 +0700

    model

--- user_management/models.py
+++ user_management/models.py
@@ -58,7 +58,7 @@ class User(AbstractBaseUser):
   objects = UserManager()
 
   USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = ['db', 'url','key']
+  REQUIRED_FIELDS = ['db', 'url']
 
   def __str__(self):
       return self.email
