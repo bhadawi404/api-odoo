d33159ca9567ce730e38cff744915e3a8e4c3ca0
bhadawi404 <abadawi.911@gmail.com>
2022-12-28 22:53:36 +0700

    user management

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -5,13 +5,13 @@ import xmlrpc.client
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields=['username', 'db', 'url','password','company_name','location_name']
+        fields=['email', 'db', 'url','password','company_name','location_name']
         
     
     def validate(self, attrs):
         db = attrs.get('db')
         url = attrs.get('url')
-        username = attrs.get('username')
+        email = attrs.get('email')
         password = attrs.get('password')
         location_name = attrs.get('location_name')
         company_name = attrs.get('company_name')
@@ -22,3 +22,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         return User.objects.create_user(**validate_data)
 
+class UserLoginSerializer(serializers.ModelSerializer):
+  email = serializers.EmailField(max_length=255)
+  class Meta:
+    model = User
+    fields = ['email', 'password']
